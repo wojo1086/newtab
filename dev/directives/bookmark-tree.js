5fa5ce9fdf94a6c73d379b209d56464fffbd2f90
Timothy Wojtylak <t.wojtylak@gmail.com>
2018-10-05 22:31:26 -0400

    added favicons

--- dev/directives/bookmark-tree.js
+++ dev/directives/bookmark-tree.js
@@ -17,7 +17,16 @@
 		function bookmarkTemplate(element, attrs) {
 			console.log(element, attrs);
 			var bookmarks = attrs.bookmarks;
-			var template = '<ul><li ng-repeat="item in bookmarks">{{item.title}} <bookmark-tree ng-if="!!item.children" bookmarks="item.children"></bookmark-tree></li></ul>';
+			var template = '<ul ng-class="{visible: true}">' +
+				'<li ng-repeat="item in bookmarks"' +
+					'ng-click="handleItemClicked($event)">' +
+						'{{(item.title === \'\') ? \'Bookmarks\' : item.title}}' +
+						'<bookmark-tree ' +
+							'ng-if="!!item.children"' +
+							'bookmarks="item.children">' +
+						'</bookmark-tree>' +
+				'</li>' +
+			'</ul>';
 
 			return template;
 		}
@@ -25,9 +34,15 @@
 		function bookmarkTreeLink(scope, elem, attrs) {
 			console.log(scope, elem, attrs);
 
+			scope.handleItemClicked = handleItemClicked;
+
 			scope.$watch('bookmarks', function(n, o) {
 				console.log(n, o);
 			});
+
+			function handleItemClicked(evt) {
+				console.log(angular.element(evt.currentTarget).find('li')[0]);
+			}
 		}
 	}
 })();
